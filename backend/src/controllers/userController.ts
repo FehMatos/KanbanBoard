702d9dffd8358bf491031b1cb4a6ff5bbaaed0f4
Feh Matos <felipempri@hotmail.com>
2026-01-12 16:06:16 -0300

    Create default columns when user signs up

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -1,6 +1,7 @@
 import User from "../models/userModel.js";
 import jwt from "jsonwebtoken";
 import { Request, Response } from "express";
+import Column from "../models/columnModel.js";
 
 interface AuthBody {
   email: string;
@@ -12,6 +13,24 @@ const createToken = (_id: string): string => {
 
   return jwt.sign({ _id }, secret, { expiresIn: "3d" });
 };
+
+const defaultColumns = [
+  {
+    title: "TO DO",
+    color: "linear-gradient(135deg, rgb(131, 149, 167), rgb(90, 107, 122))",
+    position: 0,
+  },
+  {
+    title: "DOING",
+    color: "linear-gradient(135deg, rgb(255, 159, 67), rgb(229, 138, 46))",
+    position: 1,
+  },
+  {
+    title: "DONE",
+    color: "linear-gradient(135deg, rgb(29, 209, 161), rgb(23, 165, 137))",
+    position: 2,
+  },
+];
 
 const loginUser = async (
   req: Request<{}, {}, AuthBody>,
@@ -40,6 +59,13 @@ const signupUser = async (
 
   try {
     const user = await User.signup(email, password);
+
+    await Column.insertMany(
+      defaultColumns.map((col) => ({
+        ...col,
+        user_id: user._id,
+      }))
+    );
 
     const token = createToken(user._id.toString());
 
